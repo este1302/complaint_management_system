697712e9b2f2795e335477cc6b90879294412a4c
steve <stevesoneyvarghese13@gmail.com>
2025-08-17 21:15:40 +1000

    Deploying the EC2 instance

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://localhost:5001',
+  baseURL: 'http://3.25.95.248:5001',
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
