6adb187e69985265c33918fbf29b00f1121aeaed
steve <stevesoneyvarghese13@gmail.com>
2025-08-27 12:05:40 +1000

    configuring

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001',
-  //baseURL: 'http://3.25.95.248:5001',
-  baseURL: 'http://3.107.196.75:5001',
+  baseURL: 'http://3.27.208.20:5001',
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
